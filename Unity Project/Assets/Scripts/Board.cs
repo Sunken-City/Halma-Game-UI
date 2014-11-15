1edd64e88bd2421cc1d7a994d870c9aae2b92684
RSWSMUGirl2016 <rsward@smu.edu>
2014-11-14 22:40:01 -0600

    Adding squares to the board

--- Unity Project/Assets/Scripts/Board.cs
+++ Unity Project/Assets/Scripts/Board.cs
@@ -4,42 +4,27 @@ using System.Collections;
 public class Board : MonoBehaviour {
 
 	public Transform tile;
-	public Transform square;
-	public bool floatup;
-	public float boundUp;
-	public float boundDown;
-	public float stepSize = 0.1f;
+	public Transform[,] squares = new Transform[18,18];
+	public Transform squareTwo;
 
 	// Use this for initialization
 	void Start () {
-		floatup = false;
-		square = Instantiate (tile, new Vector3 (0f, 0f, 0f), Quaternion.identity) as Transform;
-		boundUp = 2f;
-		boundDown = -2f;
+		var size = 18;
+		int i = 0;
+		int j = 0;
+		for(var x = 0; x < size; x++){
+			for(var y = 0; y < size; y++){
+				squares[i, j] = Instantiate (tile, new Vector3(x+0f, y-0.75f, 0f), Quaternion.identity) as Transform;
+				j++;
+			}
+			i++;
+			j = 0;
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (floatup)
-			floatingup ();
-		else if (!floatup) 
-			floatingdown();
+
 	}
-
-	void floatingup() {
-		var pos = square.position.y;
-		square.position += new Vector3(0f, stepSize, 0f);
-		if(pos > boundUp)
-			floatup = false;
-	}
-
-	void floatingdown() {
-		var pos = square.position.y;
-		square.position -= new Vector3(0f, stepSize, 0f);
-		if(pos < boundDown)
-			floatup = true;
-	}
-
-
 
 }
