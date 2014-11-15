0d7886fe8d770fe32bb99ace3e44d3110d6b42dc
RSWSMUGirl2016 <rsward@smu.edu>
2014-11-15 15:18:01 -0600

    Playing with scale of the tiles in the script

--- Unity Project/Assets/Scripts/Board.cs
+++ Unity Project/Assets/Scripts/Board.cs
@@ -12,16 +12,20 @@ public class Board : MonoBehaviour {
 		var size = 18;
 		int i = 0;
 		int j = 0;
+		float desiredScale = 3.0f;
 		for(var x = 0; x < size; x++){
 			for(var y = 0; y < size; y++){
 				float tileWidth = (float)tile.renderer.bounds.size.x;
 				float tileHeight = (float)tile.renderer.bounds.size.y;
-				squares[i, j] = Instantiate (tile, new Vector3(x*tileWidth, y*tileHeight, 0f), Quaternion.identity) as Transform;
+				squares[i, j] = Instantiate (tile, new Vector3(x*tileWidth*desiredScale, y*tileHeight*desiredScale, 0f), Quaternion.identity) as Transform;
 				j++;
 			}
 			i++;
 			j = 0;
-		}
+		} 	
+		for(i = 0; i < size; i++)
+			for(j = 0; j < size; j++)
+    			squares[i,j].transform.localScale = new Vector3( 1f*desiredScale, 1f*desiredScale, 0f);
 	}
 	
 	// Update is called once per frame
