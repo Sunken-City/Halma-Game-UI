221ef40be6183d147c38d4f9439626d2d88ec95a
RSWSMUGirl2016 <rsward@smu.edu>
2014-11-14 21:37:17 -0600

    Tile object and script, along with beginning board

---
+++ Unity Project/Assets/Scripts/Board.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+
+	public Transform tile;
+	public Transform square;
+	public bool floatup;
+	public int boundUp;
+	public int boundDown;
+
+	// Use this for initialization
+	void Start () {
+		floatup = false;
+		square = Instantiate (tile, new Vector3 (0f, 0f, 0f), Quaternion.identity) as Transform;
+		boundUp = 2;
+		boundDown = -2;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var pos = square.position.y;
+		if (boundUp < pos && floatup)
+			floatingup ();
+		else if (boundDown < pos && !floatup) 
+			floatingdown();
+	}
+
+	void floatingup() {
+		square.position += new Vector3(0f, 0.3f, 0f);
+		floatup = false;
+	}
+
+	void floatingdown() {
+		square.position -= new Vector3(0f, 0.3f, 0f);
+		floatup = true;
+	}
+
+
+
+}
