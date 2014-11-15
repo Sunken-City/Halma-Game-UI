b055d11c19efa8fce16e48841001b4f89edea5fd
picoriley <acloudy@mail.smu.edu>
2014-11-15 16:04:39 -0600

    Implemented a scrolling background with a controller

--- Unity Project/Assets/Scripts/backgroundScroll.cs
+++ Unity Project/Assets/Scripts/backgroundScroll.cs
@@ -3,13 +3,18 @@ using System.Collections;
 
 public class backgroundScroll : MonoBehaviour {
 
+	private float halfLength;
+	public float scrollSpeed = 0.005f;
 	// Use this for initialization
 	void Start () {
-	
+		halfLength = (float)this.renderer.bounds.size.x / 2.0f;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		this.GetComponent<Transform> ().position -= new Vector3 (0.005f, 0f, 0f);
+		this.GetComponent<Transform> ().position -= new Vector3 (scrollSpeed, 0f, 0f);
+		//if (this.transform.position.x - halfLength < Camera.main.ViewportToWorldPoint (new Vector3 (1, 1, 0)).x)
+						//this.GetComponent<Transform> ().position = new Vector3 (3f, 0f, 0f);
+
 	}
 }
