be81b65401af35b2e603e3605d0e6c7abe73cd67
picoriley <acloudy@mail.smu.edu>
2014-11-26 16:19:54 -0600

    The Player entity now instantiates the pieces!

--- Unity Project/Assets/Scripts/Player.cs
+++ Unity Project/Assets/Scripts/Player.cs
@@ -3,15 +3,22 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 
+	public Vector3 originLocation;
+	public Transform Piece;
+	private ArrayList pieces;
 	// Use this for initialization
 	void Start () {
-	
+		originLocation = new Vector3 (0f, 0f, 0f);
 	}
 
 	// Use this to pass parameters to the player entity
 	public void Initialize(ArrayList startingLocations) {
-		Debug.Log ("hello");
-		Debug.Log (startingLocations);
+		pieces = new ArrayList ();
+		foreach (Vector2 location in startingLocations) {
+			Transform piece = Instantiate(Piece, new Vector3 (0f, 0f, 0f), Quaternion.identity) as Transform; 
+			piece.GetComponent<Piece>().Initialize(location, originLocation, 1.0f);
+			pieces.Add(piece);
+		}
 	}
 
 	// Update is called once per frame
