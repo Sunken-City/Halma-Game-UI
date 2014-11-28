459e374d1a665ee314be73b6e9822d5e29d7cf52
picoriley <acloudy@mail.smu.edu>
2014-11-28 17:22:15 -0600

    Created a test case for saving data from the menu screen

---
+++ Unity Project/Assets/Scripts/menuController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class menuController : MonoBehaviour {
+
+	public GameObject player1Name;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void saveInfo()
+	{
+		Debug.Log(player1Name.GetComponent<Text>().text);
+		PlayerPrefs.SetString ("Player1Name", player1Name.GetComponent<Text>().text);
+		Debug.Log(PlayerPrefs.GetString ("Player1Name"));
+	}
+}
