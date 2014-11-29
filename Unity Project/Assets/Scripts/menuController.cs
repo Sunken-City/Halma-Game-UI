1d4783d336e8529018da6128c04c88b65492aed1
picoriley <acloudy@mail.smu.edu>
2014-11-29 01:32:51 -0600

    You can no longer go to the next screen without filling out all information

--- Unity Project/Assets/Scripts/menuController.cs
+++ Unity Project/Assets/Scripts/menuController.cs
@@ -6,6 +6,9 @@ using System;
 public class menuController : MonoBehaviour {
 
 	public GameObject player1Name;
+	public GameObject player2Name;
+	public GameObject player1URL;
+	public GameObject player2URL;
 	// Use this for initialization
 	void Start () {
 		this.GetComponent<AudioSource>().Play();
@@ -19,15 +22,24 @@ public class menuController : MonoBehaviour {
 	public void saveInfo()
 	{
 		string player1NameText = player1Name.GetComponent<Text>().text;
-		if (player1NameText != "")
+		string player2NameText = player2Name.GetComponent<Text>().text;
+		string player1URLText = player1URL.GetComponent<Text>().text;
+		string player2URLText = player2URL.GetComponent<Text>().text;
+		if (player1NameText == "" || player2NameText == "" || player1URLText == "" || player2URLText == "")
 		{
-			PlayerPrefs.SetString ("Player1Name", player1Name.GetComponent<Text>().text);
-			Debug.Log(PlayerPrefs.GetString ("Player1Name"));
-			Application.LoadLevel ("gameScene");
+			return;
 		}
 		else 
 		{
-			Debug.Log ("Nope.avi");
+			PlayerPrefs.SetString ("Player1Name", player1Name.GetComponent<Text>().text);
+			Debug.Log(PlayerPrefs.GetString ("Player1Name"));
+			PlayerPrefs.SetString ("player2Name", player2Name.GetComponent<Text>().text);
+			Debug.Log(PlayerPrefs.GetString ("player2Name"));
+			PlayerPrefs.SetString ("player1URL", player1URL.GetComponent<Text>().text);
+			Debug.Log(PlayerPrefs.GetString ("player1URL"));
+			PlayerPrefs.SetString ("player2URL", player2URL.GetComponent<Text>().text);
+			Debug.Log(PlayerPrefs.GetString ("player2URL"));
+			Application.LoadLevel ("gameScene");
 		}
 	}
 }
